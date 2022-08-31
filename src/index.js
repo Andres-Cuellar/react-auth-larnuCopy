cd022ba18e38af226f8df97d2b6ea9e96cfa737e
cirkuit32 <emilianorottaf@gmail.com>
2022-08-31 17:15:45 -0300

    Ejercicio React-Auth

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ import TodoContextProvider from './contexts/todoContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserContextProvider>
-      <TodoContextProvider>
-        <App />
-      </TodoContextProvider>
-    </UserContextProvider>
+    <App />
   </React.StrictMode>
 );
 
