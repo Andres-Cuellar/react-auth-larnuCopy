d7a80ee65799da38ec056e70d5ae727f2115f120
cirkuit32 <emilianorottaf@gmail.com>
2022-08-31 17:07:05 -0300

    Ejercicio React-Auth

--- src/App.js
+++ src/App.js
@@ -103,28 +103,3 @@ function App() {
   )
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-  // const { logged } = useContext(UserContext);
-  
-
-  // useEffect(()  => {
-  //   // checkear si existe un token en el localStorage
-  //   // si no existe, mostrar formulario de login
-  //   // checkear si ese token es valido (endpoint check)
-  //   // si es valido, setear logged a true
-  //   // si no es valido, eliminar token de localStorage mostrar formulario de login
-  
-  // });
-  
-
-
-
