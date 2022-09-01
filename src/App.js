68adde0f7acb844146fd6b05fb05a080d3b32ec0
vizmarvizcaino <gatome2000@hotmail.com>
2022-09-01 10:57:00 -0500

    react_auth

--- src/App.js
+++ src/App.js
@@ -1,105 +1,26 @@
-import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useContext } from "react";
+import { RequestContextPost } from "./context/RequestContextPost";
+import { RequestContextPostProvider } from "./context/RequestContextPost";
 import './App.css';
+import Form from "./compnents/Formulario";
+import Welcome from './compnents/PeticionGet';
+
 
 function App() {
+  const logged  = useContext(RequestContextPost)
 
-  const baseUrl = 'https://ms-discord-upy5mhs63a-rj.a.run.app';
-  
-  const [values, setValues] = useState({
-    email: "jose@larnu.com",
-    discordId: "310544245155168256"
-  });
-  const [token, setToken] = useState(null);
-  const [logged, setLogged] = useState(false);
-  
-  
-  const handleChange = (event) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [event.target.name]: event.target.value
-    }));
-  }
-
-  function login() {
-    axios.post(`${baseUrl}/auth/login`, values)
-      .then(res=> res.data.token)
-      .then(token => {
-        setToken(token)
-        setLogged(true)
-      })
-  }
-  
-
-
-  function submit(e) {
-    e.preventDefault();
-    login();
-  }
-
-  const Form = () =>{
-    return (
-      <form className='form' onSubmit={submit}>
-        <div>
-          <label>email</label>
-          <input 
-            type="email"
-            name='email'
-            value={values.email || ''}
-            onChange={handleChange}
-            placeholder='email'
-            required
-          />
-        </div>
-        <div>
-          <label>discordId</label>
-          <input 
-            type="text"
-            name='discordId'
-            value={values.discordId || ''}
-            onChange={handleChange}
-            placeholder='Discord Id'
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-
-  const Welcome = () => {
-
-    const [checked, setChecked] = useState(false);
-    const [data, setData] = useState(null);
-
-    async function checkToken () {
-      axios.get(`${baseUrl}/auth/check`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }).then (res => {
-        setData(res.data);
-        setChecked(true);
-      })
-    }
-
-    useEffect(()  => {
-      checkToken();
-    }, []);  
-
-    return ( 
-      <>
-        { checked ? <div>Bienvenido {data.username}, tu numero de Usuario es: {data.id} </div> : <div>Loading...</div>}
-      </>
-      )
-  }
+  console.log(logged)
 
   return (
-    <div className="App">
-      {
-        logged ? <Welcome/> : <Form/>
-      }
-    </div>
+    
+    <RequestContextPostProvider>
+      <div className="App">
+        <h1>Ejercicio en clase utilizando useContext ('contiene error')</h1>
+        {
+         logged ? <Welcome/> : <Form />
+        }
+      </div>
+    </RequestContextPostProvider>
   )
 }
 export default App;
