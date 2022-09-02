b19a396086407120198ca39e5279041bb473e508
Cuellar Dev <afca133@gmail.com>
2022-09-02 16:35:47 -0500

    modificacion de funciones de acceso

--- src/compnents/Formulario.js
+++ src/compnents/Formulario.js
@@ -1,35 +1,49 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { RequestContextPost } from "../context/RequestContextPost";
 
+const Form = () => {
+  const { login, logged } = useContext(RequestContextPost);
 
-const Form = () =>{
-    const { submit, values, handleChange } = useContext(RequestContextPost)
-    return (
-      <form className='form' onSubmit={submit}>
-        <div>
-          <label>email</label>
-          <input 
-            type="email"
-            name='email'
-            value={values.email || ''}
-            onChange={handleChange}
-            placeholder='email'
-            required
-          />
-        </div>
-        <div>
-          <label>discordId</label>
-          <input 
-            type="text"
-            name='discordId'
-            value={values.discordId || ''}
-            onChange={handleChange}
-            placeholder='Discord Id'
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-  export default Form;
+  const [email, setEmail] = useState("");
+  const [discordId, setDiscordId] = useState("");
+
+  const data = {
+    email,
+    discordId,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(data);
+  };
+
+  console.log(logged);
+  return (
+    <form className="form">
+      <div>
+        <label>email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+          required
+        />
+      </div>
+      <div>
+        <label>discordId</label>
+        <input
+          type="text"
+          name="discordId"
+          value={discordId}
+          onChange={(e) => setDiscordId(e.target.value)}
+          placeholder="Discord Id"
+          required
+        />
+      </div>
+      <button onClick={handleSubmit}>Login</button>
+    </form>
+  );
+};
+export default Form;
--- src/context/RequestContextPost.js
+++ src/context/RequestContextPost.js
@@ -1,54 +1,36 @@
-import {React, createContext, useState} from 'react'
+import { React, createContext, useState } from "react";
 import axios from "axios";
 
-export const RequestContextPost = createContext()
+export const RequestContextPost = createContext();
 
 export const RequestContextPostProvider = (props) => {
+  const baseUrl = "https://ms-discord-upy5mhs63a-rj.a.run.app";
 
-  const baseUrl = 'https://ms-discord-upy5mhs63a-rj.a.run.app';
-  
-  const [values, setValues] = useState({
-    email: "jose@larnu.com",
-    discordId: "310544245155168256"
-  });
   const [token, setToken] = useState(null);
   const [logged, setLogged] = useState(false);
 
-  
-  
-  const handleChange = (event) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [event.target.name]: event.target.value
-    }));
-  }
-
-  function login() {
-    axios.post(`${baseUrl}/auth/login`, values)
-      .then(res=> res.data.token)
-      .then(token => {
-        setToken(token)
-        setLogged(true)
+  function login(data) {
+    axios
+      .post(`${baseUrl}/auth/login`, data)
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.token));
       })
+      .then((token) => {
+        setToken(token);
+        setLogged(true);
+      });
   }
 
-
-  function submit(e) {
-    e.preventDefault();
-    login();
-  }
-    
-    return (
-      <RequestContextPost.Provider value={{
+  return (
+    <RequestContextPost.Provider
+      value={{
         baseUrl,
-        values,
         token,
         logged,
-        handleChange,
         login,
-        submit
-      }}>
-          {props.children}
-      </RequestContextPost.Provider>
-    )
-  }
+      }}
+    >
+      {props.children}
+    </RequestContextPost.Provider>
+  );
+};
